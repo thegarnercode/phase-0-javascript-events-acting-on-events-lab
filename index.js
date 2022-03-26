2f307dbc88ffee8be15802c3d30841e4983c16af
Denisha Garner <thegarnercode@gmail.com>
2022-03-26 00:59:49 -0400

    commit tree 6bfd72aafa8103ebb602c7c2608c212ae39b99a1

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Your code here
+const dodger = document.getElementById("dodger");// Your code here
+
+dodger.style.backgroundColor = "red";
+
+dodger.style.bottom = "0px";
+dodger.style.left = "0px";
+
+document.addEventListener("keydown", function (event) {
+    if (event.key === "ArrowLeft") {
+      const leftNumbers = dodger.style.left.replace("px", "");
+      const left = parseInt(leftNumbers, 10);
+  
+      dodger.style.left = `${left - 2}px`;
+    }
+  });
+
+
+function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`;
+    }
+  }
+  
+  document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowLeft") {
+      moveDodgerLeft();
+    }
+  });
+
+  
